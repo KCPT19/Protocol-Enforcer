a7d37288fc64ab941232f7d59847907823e4f1ee
Steven <skohlmeyer@kcpt.org>
2016-06-16 16:38:53 +0100

    Fully working plugin now

--- force-protocol-by-post.php
+++ force-protocol-by-post.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * Plugin Name: Force Protocol By Post
  * Author: Fastmover
  * Description: Plugin to allow user to specify by post, whether to force http or https.
  * Text Domain: kcptforceprotocol
  */
-
 class KCPT_Force_Protocol
 {
 
@@ -14,15 +14,67 @@ class KCPT_Force_Protocol
     public function __construct()
     {
 
-        add_action( 'add_meta_boxes', array( $this, 'metabox' ) );
+        add_action( 'add_meta_boxes', [ $this, 'metabox' ] );
         add_action( 'save_post', [ $this, 'saveMetabox' ] );
+        add_action( 'template_redirect', [ $this, 'redirect' ] );
 
     }
 
     public function metabox()
     {
 
-        add_meta_box( $this->slug, __( 'Force Protocol', 'kcptforceprotocol' ), array( $this, 'viewMetabox' ), null, 'side', 'high' );
+        add_meta_box( $this->slug, __( 'Force Protocol', 'kcptforceprotocol' ), [ $this, 'viewMetabox' ], null, 'side', 'high' );
+
+    }
+
+    public function redirect()
+    {
+
+        if (is_home() or is_category() or is_archive() or is_feed())
+            return;
+
+        global $post;
+
+        if ( ! isset( $post ) or ! isset( $post->ID ))
+            return;
+
+        $meta     = get_post_custom( $post->ID );
+        $option   = false;
+        $protocol = "http";
+
+        if (isset( $meta[ 'forceprotocol' ][ 0 ] ) and ! empty( $meta[ 'forceprotocol' ][ 0 ] )) {
+            $option = $meta[ 'forceprotocol' ][ 0 ];
+        }
+
+        if ( ! $option) return;
+
+//        var_dump( $GLOBALS ); die;
+
+        if (isset( $_SERVER[ 'HTTPS' ] ) and $_SERVER[ 'HTTPS' ] == "on")
+            $protocol = "https";
+
+        if (isset( $_SERVER[ 'REQUEST_SCHEME' ] ) and $_SERVER[ 'REQUEST_SCHEME' ] == "https")
+            $protocol = "https";
+
+        switch ( $option ) {
+            case 'http':
+                if ($protocol == "https") {
+                    $URL = $option . '://' . $_SERVER[ 'HTTP_HOST' ] . $_SERVER[ 'REQUEST_URI' ];
+                    wp_safe_redirect( $URL, 301 );
+                    exit;
+                }
+                break;
+            case 'https':
+                if ($protocol == "http") {
+                    $URL = $option . '://' . $_SERVER[ 'HTTP_HOST' ] . $_SERVER[ 'REQUEST_URI' ];
+                    wp_safe_redirect( $URL, 301 );
+                    exit;
+                }
+                break;
+        }
+
+        return;
+
 
     }
 
@@ -48,12 +100,12 @@ class KCPT_Force_Protocol
             return $postID;
         }
 
-        if( ! isset( $_POST[ 'forceprotocol' ] ) )
+        if ( ! isset( $_POST[ 'forceprotocol' ] ))
             return $postID;
 
         $option = sanitize_text_field( $_POST[ 'forceprotocol' ] );
 
-        if( ! empty( $option ) )
+        if ( ! empty( $option ))
             update_post_meta( $postID, 'forceprotocol', $option );
 
         return $postID;
@@ -70,20 +122,22 @@ class KCPT_Force_Protocol
 
         $option = "default";
 
-        if ( isset( $meta[ 'forceprotocol' ][ 0 ] ) and ! empty( $meta[ 'forceprotocol' ][ 0 ] )) {
-            $option = $meta[ 'forceprotocol' ][0];
+        if (isset( $meta[ 'forceprotocol' ][ 0 ] ) and ! empty( $meta[ 'forceprotocol' ][ 0 ] )) {
+            $option = $meta[ 'forceprotocol' ][ 0 ];
         }
 
 //        var_dump( $meta );
 
         ?>
-        <input type="hidden" name="kcpt_metabox_protocol_nonce" value="<?= wp_create_nonce( basename( __FILE__ ) ); ?>"/>
+        <input type="hidden" name="kcpt_metabox_protocol_nonce"
+               value="<?= wp_create_nonce( basename( __FILE__ ) ); ?>"/>
         <select name="forceprotocol">
-            <option value="default" <?php if( $option == "default" ): ?>selected="selected"<?php endif; ?>>Default</option>
-            <option value="http" <?php if( $option == "http" ): ?>selected="selected"<?php endif; ?>>HTTP</option>
-            <option value="https" <?php if( $option == "https" ): ?>selected="selected"<?php endif; ?>>HTTPS</option>
+            <option value="default" <?php if ( $option == "default" ): ?>selected="selected"<?php endif; ?>>Default
+            </option>
+            <option value="http" <?php if ( $option == "http" ): ?>selected="selected"<?php endif; ?>>HTTP</option>
+            <option value="https" <?php if ( $option == "https" ): ?>selected="selected"<?php endif; ?>>HTTPS</option>
         </select>
-<?php
+        <?php
 
     }
 
